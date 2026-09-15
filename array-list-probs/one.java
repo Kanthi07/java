9c89fd3f0f9b312536d2ca9985e7e9e409c596a3
Maverick <mavericksentinel2@gmail.com>
2026-09-16 00:23:22 +0530

    changed name from arrayList to this, because it was creating packages and was considered as a java package

---
+++ array-list-probs/one.java
@@ -0,0 +1,27 @@
+package arrayList;
+
+import java.util.ArrayList;
+
+public class one {
+    public static void main(String[] args){
+        ArrayList<Integer> grades=new ArrayList<>();
+        grades.add(1);
+        grades.add(2);
+        grades.add(2);
+        grades.add(4);
+        grades.add(5);
+        int sum=0;
+        for(int grade:grades){
+            sum+=grade;
+        }
+        int average=(sum/grades.size());
+        System.out.println(average);
+        System.out.println(grades);
+        System.out.println(grades.size());
+
+
+        
+
+
+    }
+}
